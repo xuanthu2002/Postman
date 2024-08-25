e1f6a4c23d898bf7543fa9ce0a5863019dd09e3f
xuanthu2002 <tranxuanthu3721@gmail.com>
2024-08-26 00:44:51 +0700

    Enhance Response Body Formatting for Better Readability

--- src/main/java/postman/gui/components/TextEditor.java
+++ src/main/java/postman/gui/components/TextEditor.java
@@ -1,14 +1,31 @@
 package postman.gui.components;
 
 import org.fife.ui.rsyntaxtextarea.RSyntaxTextArea;
+import org.fife.ui.rsyntaxtextarea.Theme;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-import static postman.gui.constants.Fonts.GENERAL_PLAIN_12;
+import java.awt.*;
+import java.io.IOException;
+
 import static postman.gui.constants.Values.TAB_SIZE;
 
 public class TextEditor extends RSyntaxTextArea {
+
+    private static final Logger log = LoggerFactory.getLogger(TextEditor.class);
+
     public TextEditor() {
         super();
-        this.setFont(GENERAL_PLAIN_12);
+        try {
+            Theme theme = Theme.load(getClass().getResourceAsStream(
+                    "/org/fife/ui/rsyntaxtextarea/themes/idea.xml"));
+            theme.apply(this);
+        } catch (IOException ioe) {
+            log.error("Unable to set TextEditor theme", ioe);
+        }
+        Font font = this.getFont();
+        font = new Font(font.getFontName(), font.getStyle(), 12);
+        this.setFont(font);
         this.setTabSize(TAB_SIZE);
         this.setTabsEmulated(true);
         this.setAntiAliasingEnabled(true);
